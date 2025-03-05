3b6e9d394716e751f2080e6922bd81ba42a0facb
RobyLedda94 <robyledda01@yahoo.it>
2025-03-05 18:23:35 +0100

    [UPD] definito array di oggetti per il team e importate immagini

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-console.log('Prova Link');
+// Defnizione array di oggetti per il team
+
+const array_team = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & Ceo",
+        img: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Carrol",
+        role: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        img: "walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg",
+    },
+];
